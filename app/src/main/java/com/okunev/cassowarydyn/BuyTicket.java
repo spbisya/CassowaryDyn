a3faa7e06d0cfa74aaedd158b46724731ecff6f2
spbisya <wtcute.info@gmail.com>
2016-02-20 08:11:52 +0300

    Added TicketController Screen

--- app/src/main/java/com/okunev/cassowarydyn/BuyTicket.java
+++ app/src/main/java/com/okunev/cassowarydyn/BuyTicket.java
@@ -1,5 +1,24 @@
 package com.okunev.cassowarydyn;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import no.agens.cassowarylayout.CassowaryLayout;
+import no.agens.cassowarylayout.ViewIdResolver;
+
 /**
  * Created by gwa on 2/19/16.
  */
